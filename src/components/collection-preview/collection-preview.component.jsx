6af795e53a57234a39592c870e25fda3515361bf
Culver Lau <culverlau@gmail.com>
2019-12-24 13:39:54 -0500

    shop page, collection page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import "./collection-preview.styles.scss"
+import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-      .filter((item, idx) => idx < 4)
-      .map(item => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...rest }) => (
+          <CollectionItem key={id} {...rest} />
+        ))}
     </div>
   </div>
-)
+);
 
-export default CollectionPreview
+export default CollectionPreview;
